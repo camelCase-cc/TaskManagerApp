889242cf4a39a1e01cd87f57dc5d3069bd9f6e95
Camel Case <116927290+camelCase-cc@users.noreply.github.com>
2023-06-08 13:29:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 //dyanamically generate html elements to display each task on the page
 //Update the task list based on user action
 
-const taskLists = []; //storing the tasks
-// const completedTasks = []; //an array to store completed tasks
-// const uncompletedTasks = []; //an array to store uncomleted tasks
+var taskLists = []; //storing the tasks
 
 const tasks = document.querySelector('.tasks'); //selects the element with the class tasks
 const overlay = document.querySelector('.overlay'); //selects the element with a class of overlay
@@ -12,23 +10,23 @@ const modal = document.querySelector('.modal'); //selects the element with a cla
 const openModalDiv = document.querySelector('.open-modal-div'); //opens the modal
 const closeModalBtn = document.querySelector('.close')
 const taskName = document.getElementById('input-task'); //stores the name of the task
-const taskCategory = document.getElementById('input-category'); //stores the category of the task
 const addTaskBtn= document.getElementById('add-task'); //adds a new task to the list
 const deleteBtn = document.querySelector('.delete-task'); //deletes a task from the list
 
 //removes the class 'hidden' from the modal and overlay
-const openModal = function() {
+const openModal = () => {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
+    taskName.focus();
 }
 
 //adds the class 'hidden' to the modal and overlay
-const closeModal = function(e) {
+const closeModal = (e) => {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-const findIndex = function(taskId) {
+const findIndex = (taskId) => {
     let targetIndex = -1;
     for (i = 0 ; i < taskLists.length ; i ++) {
         if (taskLists[i].id == taskId) {
@@ -38,33 +36,31 @@ const findIndex = function(taskId) {
     return targetIndex;
 }
 
-openModalDiv.addEventListener('click', openModal); //opens the modal when clicked
-closeModalBtn.addEventListener('click', closeModal); //closes the modal when clicked
-addTaskBtn.addEventListener('click', validateInput); //adds the task when clicked
-tasks.addEventListener('click', function(e) {
-    if (e.target.classList.contains('mark-as-complete')) {
-        const itemKey = e.target.parentElement.id;
-        console.log(itemKey);
-        markAsComplete(itemKey);
-        console.log(taskLists);
-    } else if (e.target.classList.contains('delete-task')) {
-        const itemKey = e.target.parentElement.id;
-        console.log(itemKey);
-        deleteTask(itemKey);
-        console.log(taskLists);
+//validate the users input
+const validateInput = () => {
+    let text = taskName.value; // Get the value of the task input
+    event.preventDefault(); // prevent page refresh on form submission
+    
+    //checks to see if the text is empty or not
+    if (text !== '') {
+        taskName.value = '';
+        addTask(text); //parses 'text, description, date, category' into the add task function
+        closeModal();
+    } else {
+        alert('Field cannot be empty!');
     }
-});
-document.addEventListener('keydown', handleClick);
-window.addEventListener('click', handleClick);
+}
 
 //creates a new todo objects with following properties 'name, category, isComplete, id, date, description' when called parses the taskName and Category property into the task object
-function addTask (taskName, taskDescription, taskDate, taskCategory) {
-    const task = { name: taskName, category: taskCategory, isComplete: false, deleted: false, id: Date.now() };
+const addTask = (taskName, taskDescription, taskDate, taskCategory) => {
+    const task = { name: taskName, isComplete: false, deleted: false, id: Date.now() };
     taskLists.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskLists));
     displayTask(task);
 }
 
-function displayTask(task) {
+const displayTask = (task) => {
+    localStorage.setItem('tasks', JSON.stringify(taskLists));
     const list = document.querySelector('.tasks'); //selects the ul element with class 'tasks'
     const item = document.querySelector(`[id='${task.id}']`);
 
@@ -82,6 +78,7 @@ function displayTask(task) {
     newTodoItem.innerHTML = `
         <label class='label-text' id='${task.id}'>${task.name}</label>
         <span class='buttons' id='${task.id}'>
+            <span class='edit-btn'>&#9998;</span>
             <input type='button' value = '✓'name='${task.id}' id='${task.id}' class='mark-as-complete'>
             <input type='button' value='x' name='${task.id}' id='${task.id}' class='delete-task'>
         </span>
@@ -94,49 +91,28 @@ function displayTask(task) {
     }
 }
 
-function markAsComplete(taskId) {
+const markAsComplete = (taskId) => {
     const index = findIndex(taskId);
     taskLists[index].isComplete = !taskLists[index].isComplete;
     displayTask(taskLists[index]);
 }
 
-function editTask() {
-
+const editTask = (itemKey) => {
 }
 
-function deleteTask(taskId) {
+const deleteTask = (taskId) => {
     const index = findIndex(taskId);
     if(index > -1 && index < taskLists.length) {
-        taskLists[index].deleted = !taskLists[index.deleted]
+        taskLists[index].deleted = !taskLists[index.deleted];
         displayTask(taskLists[index]);
         taskLists.splice(index, 1);
+        localStorage.setItem('tasks', JSON.stringify(taskLists));
     } else {
         alert("invalid index specified")
     }
 }
 
-//validate the users input
-function validateInput() {
-    let text = taskName.value; // Get the value of the task input
-    event.preventDefault(); // prevent page refresh on form submission
-    
-    //checks to see if the text is empty or not
-    if (text !== '') {
-        addTask(text); //parses 'text, description, date, category' into the add task function
-        setToDefault();
-        closeModal();
-    } else {
-        alert('Field cannot be empty!');
-    }
-}
-
-//reset all the input value back to default, i.e, ' '
-function setToDefault() {
-    taskName.value = '';
-    taskName.focus();
-}
-
-function handleClick(event) {
+const handleClick = (event) => {
     if (event.target.classList.contains('overlay')) {
         closeModal(); //if anywhere outside the modal is clicked, call the close modal function
     } if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
@@ -145,3 +121,30 @@ function handleClick(event) {
         validateInput(); //if enter button is clicked, call the add task method
     }
 }
+
+openModalDiv.addEventListener('click', openModal); //opens the modal when clicked
+closeModalBtn.addEventListener('click', closeModal); //closes the modal when clicked
+addTaskBtn.addEventListener('click', validateInput); //adds the task when clicked
+tasks.addEventListener('click', function(e) {
+    if (e.target.classList.contains('mark-as-complete')) {
+        const itemKey = e.target.parentElement.id;
+        markAsComplete(itemKey);
+    } else if (e.target.classList.contains('delete-task')) {
+        const itemKey = e.target.parentElement.id;
+        deleteTask(itemKey);
+    } else if (e.target.classList.contains('edit-btn')) {
+        const itemKey = e.target.parentElement.id;
+        editTask(itemKey);
+    }
+});
+document.addEventListener('keydown', handleClick);
+window.addEventListener('click', handleClick);
+document.addEventListener('DOMContentLoaded', function() {
+    const localStorageData = localStorage.getItem('tasks');
+    if (localStorageData) {
+        taskLists = JSON.parse(localStorageData);
+        taskLists.forEach (task => {
+            displayTask(task);
+        })
+    }
+});
